c544010c076fa60c7660c69de83a0fee15214891
Fabrício Galende Marques de Carvalho <fabriciogmc@gmail.com>
2022-09-05 16:48:32 -0300

    Exemplo de utilização do EJS com inclusão de arquivo, condicional e laço

---
+++ templates_ejs/src/app.js
@@ -0,0 +1,42 @@
+/*
+Exemplo simples de projeto com uma estrutura
+de diretórios organizada.
+Autor: Fabrício G. M. de Carvalho, Ph.D
+*/
+
+/* importando o express */
+const express = require('express')
+const app = express();
+const port = 5000;
+
+/* importando o modelo */
+const modelo = require('./models/modelos');
+var Projeto = modelo.Projeto; //Vinculação de tipo
+
+
+/* Configurando a template engine. */
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+/* Configurando o diretório que serve arquivos estáticos.*/
+app.use(express.static('public'));
+
+app.get('/', listProjectHandler);
+
+app.listen(port, listenHandler);
+
+function listProjectHandler(req, res){
+    /* Os dados a seguir, em uma aplicação real, deveriam vir de um BD */
+    let projeto_1 = new Projeto("software","JavaScript", 2020, 2023); 
+    let projeto_2 = new Projeto("social","",2020,2021);
+    let projeto_3 = new Projeto("software","HTML", 2021, 2021);    
+    let projetos = [];
+    projetos.push(projeto_1);
+    projetos.push(projeto_2);
+    projetos.push(projeto_3);
+    res.render('listar_projetos.ejs',{lista_projetos: projetos});    
+}
+
+function listenHandler(){
+    console.log(`Escutando na porta ${port}!`);
+}
---
+++ templates_ejs/src/models/modelos.js
@@ -0,0 +1,18 @@
+class Projeto{
+    constructor(tipo, tecnologia, inicio, fim){
+        this.tipo = tipo;
+        this.tecnologia = tecnologia;
+        this.inicio = inicio;
+        this.fim = fim;
+
+    }
+}
+
+module.exports = {
+    Projeto: Projeto
+}  
+
+
+
+
+
